8a07e9e3cdc20923752b278ed464d7951ded9d84
lucka <kurova@mujdok.cz>
2019-10-05 00:10:15 +0200

    change app.test.js 0 to 1

--- src/components/Checkbox/__tests__/Checkbox.test.js
+++ src/components/Checkbox/__tests__/Checkbox.test.js
@@ -15,7 +15,7 @@ it('should toggle isChecked state when input change', () => {
   const input = wrapped.find('input');
 
   /* isChecked should start with false */
-  expect(wrapped.state().isChecked).toEqual(true); //false
+  expect(wrapped.state().isChecked).toEqual(false); //false
   input.simulate('change');
   /* Then toggle to true */
   expect(wrapped.state().isChecked).toEqual(true);
